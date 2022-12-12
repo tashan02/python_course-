cdaf38bc21f2eee656794099aed6ab48f40a9c6f
Nataliya Pavlova <baru5hnikova123@yandex.ru>
2022-12-12 23:04:18 +0500

    1
    1
    
    Co-Authored-By: Nataliya Pavlova <69388628+tashan02@users.noreply.github.com>

--- Проект питон/venv/app.py
+++ Проект питон/venv/app.py
@@ -1,65 +1,38 @@
+import os
 import flask
-from data_processing import exp_run_average_adaptive, kalman, median, one_sample, read_csv, run_middle, stret_sample
-import matplotlib.pyplot as plt
+from flask import render_template, redirect, url_for, request
+import data_processing as dpr
 
 
-app = flask.Flask('Проект по питону', static_folder= 'static')
+#from flask import Blueprint
 
-def draw_graph(filename:str, data, data2):
-    plt.switch_backend('agg')
-    plt.plot(data)
-    if data2:
-        plt.plot(data2)
-    image_filename = f'/static/temp/images/{filename}.png'
-    plt.savefig(f'.{image_filename}')
-    return flask.render_template("index.html", image_filename=image_filename)
+
+app = flask.Flask('Проект по питону', static_folder= 'data_processing.py')
+#app = Blueprint('urlToHash', __name__, template_folder='templates')
+
+#@app.route('/<filename>/', methods =['GET', 'POST'])
+#def index(filename:str):
+#    q = request.form['q']
+    #return render_template('index.html') - - work 
+
+   # return render_template("index.html")
+#def show_index_page():
+#    return 'Hello'
 
 @app.get('/<filename>/')
-def simple_graph(filename:str):
-    data = read_csv(f'./data/{filename}.txt')
-    return draw_graph(f'{filename}_simple', data)
+def process_word(filename:str):
+    #return dpr.finde_all(f'./data/{filename}.txt').to_dict()
+    tweets = list(dpr.finde_all(f'./data/{filename}.txt').to_dict().values())
+    return flask.render_template("index.html", tweets = tweets)
+    
+#@app.route('/', methods=["GET"])
+#def index():
+ 
+#   return render_template("index.html", messages = messages)
+ 
 
-@app.get('/<filename>/one_sample')
-def one_sample_graph(filename:str):
-    data = read_csv(f'./data/{filename}.txt')
-    calc = one_sample()
-    data2 = [calc(x) for x in data]
-    return draw_graph(f'{filename}_one_sample', data, data2)
-
-@app.get('/<filename>/stret_sample')
-def stret_sample_graph(filename:str):
-    data = read_csv(f'./data/{filename}.txt')
-    calc = stret_sample()
-    data2 = [calc(x) for x in data]
-    return draw_graph(f'{filename}_stret_sample', data, data2)
-
-@app.get('/<filename>/run_middle')
-def run_middle_graph(filename:str):
-    data = read_csv(f'./data/{filename}.txt')
-    calc = run_middle()
-    data2 = [calc(x) for x in data]
-    return draw_graph(f'{filename}_run_middle', data, data2)
-
-@app.get('/<filename>/exp_run_average_adaptive')
-def exp_run_average_adaptive_graph(filename:str):
-    data = read_csv(f'./data/{filename}.txt')
-    calc = exp_run_average_adaptive()
-    data2 = [calc(x) for x in data]
-    return draw_graph(f'{filename}_exp_run_average_adaptive', data, data2)
-
-@app.get('/<filename>/median')
-def median_graph(filename:str):
-    data = read_csv(f'./data/{filename}.txt')
-    calc = median()
-    data2 = [calc(x) for x in data]
-    return draw_graph(f'{filename}_median', data, data2)
-
-@app.get('/<filename>/kalman')
-def kalman_graph(filename:str):
-    data = read_csv(f'./data/{filename}.txt')
-    calc = kalman()
-    data2 = [calc(x) for x in data]
-    return draw_graph(f'{filename}_kalman', data, data2)
+ 
+ 
 
 
 
